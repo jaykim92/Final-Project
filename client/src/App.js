6f9f2c9aeeef2aaa28bbaec6da01a90c4921dde8
Sapana Chaudhary <sapana.chy23@gmail.com>
2020-08-06 10:22:29 -0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 // import Map from './components/map';
 import SedMap from './components/SedMap/SedMap.js'
+import Nav from './components/NavBar'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
-  return <SedMap/>
-  ;
+  return <>
+  <Router>
+    <Nav/><SedMap/>
+  </Router>
+  
+  </>
 }
 
 export default App;
